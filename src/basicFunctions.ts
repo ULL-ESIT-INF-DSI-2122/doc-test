71316fd92b48f5322bc86d27395d3f9c83f57724
Eduardo Segredo <esegredo@ull.edu.es>
2021-03-01 12:41:10 +0000

    New version of documentation

--- src/basicFunctions.ts
+++ src/basicFunctions.ts
@@ -1,7 +1,7 @@
 /**
  * Adds two numbers
- * @param firstNumber Consists of the first operand of the addition
- * @param secondNumber Consists of the second operand of the addition
+ * @param firstNumber Consists of the first numeric operand of the addition
+ * @param secondNumber Consists of the second numeric operand of the addition
  * @return The addition of the two numbers `firstNumber` and `secondNumber`
  *
  * Usage:
